afb80af79c91be663285f2abd852382fa9a423d4
genesisdayrit <genesisdayrit@gmail.com>
2025-10-02 11:27:11 -0400

    link to route to create-parody

--- app/src/client/pages/Home.tsx
+++ app/src/client/pages/Home.tsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
+    
+    
     return (
         <>
             <div className="flex flex-col h-screen items-center text-center justify-center">
                 <h1>Song Parody AI </h1>
-                <button className="border">Create a New Song Parody</button>
+                <button className="border">
+                    <Link to="/create-parody">
+                        Create a New Song Parody
+                    </Link>
+                </button>
+
             </div>
         </>
     )

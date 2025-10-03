f1ba50c84b0ca7ffa282665630f11fdffd4ce847
genesisdayrit <genesisdayrit@gmail.com>
2025-10-03 14:07:02 -0400

    install shadcn, change home page create parody button

--- app/src/client/pages/Home.tsx
+++ app/src/client/pages/Home.tsx
@@ -1,19 +1,15 @@
 import { Link } from 'react-router-dom';
+import { Button } from "../components/ui/button"
 
 export default function Home() {
-    
-    
     return (
-        <>
-            <div className="flex flex-col h-screen items-center text-center justify-center">
-                <h1>Song Parody AI </h1>
-                <button className="border">
-                    <Link to="/create-parody">
-                        Create a New Song Parody
-                    </Link>
-                </button>
-
-            </div>
-        </>
+        <div className="flex flex-col h-screen items-center text-center justify-center gap-4">
+            <h1 className="text-4xl font-bold">Song Parody AI</h1>
+            <Button className='border bg-black text-white' asChild>
+                <Link to="/create-parody">
+                    Create a New Song Parody
+                </Link>
+            </Button>
+        </div>
     )
 }

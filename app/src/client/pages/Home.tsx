9615b6bd065c0878dcda23b79387e799e118574d
genesisdayrit <genesisdayrit@gmail.com>
2025-10-03 15:11:53 -0400

    fix button on hover background cover

--- app/src/client/pages/Home.tsx
+++ app/src/client/pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className="flex flex-col h-screen items-center text-center justify-center gap-4">
             <h1 className="text-4xl font-bold">Song Parody AI</h1>
-            <Button className='border bg-black text-white' asChild>
+            <Button className='border bg-black text-white hover:bg-black' asChild>
                 <Link to="/create-parody">
                     Create a New Song Parody
                 </Link>
